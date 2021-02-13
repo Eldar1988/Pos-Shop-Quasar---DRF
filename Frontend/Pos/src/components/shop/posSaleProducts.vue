bc8fe01178b7b25bc1354c22cd5e16c3235c6c56
Хайбулов Эльдар <64076865+Eldar1988@users.noreply.github.com>
2021-02-13 13:24:03 +0600

    + commit

--- Frontend/Pos/src/components/shop/posSaleProducts.vue
+++ Frontend/Pos/src/components/shop/posSaleProducts.vue
@@ -1,42 +1,22 @@
 <template>
 <div>
-  <pos-products-wrapper v-if="!productsUnloaded" :products="products" />
-  <pos-skeleton-products v-else />
+  <pos-products-wrapper :products="products" />
 </div>
 </template>
 
 <script>
 import PosProductsWrapper from "components/shop/posProductsWrapper";
-import PosSkeletonProducts from "components/service/posSkeletonProducts";
 export default {
   name: "posSaleProducts",
-  components: {PosSkeletonProducts, PosProductsWrapper},
-  data() {
-    return {
-      products: [],
-      productsUnloaded: false
+  components: {PosProductsWrapper},
+  computed: {
+    products() {
+
+      return this.$store.getters.getSaleProducts
     }
   },
-  mounted() {
-    this.loadSaleProducts()
-  },
-  methods: {
-    async loadSaleProducts() {
-      try {
-        this.products = await this.$axios.get(`${this.$store.getters.getServerURL}/shop/home_sale_products/`)
-        .then(({data}) => {
-          return data.results
-        })
-      }
-      catch (e) {
-        this.productsUnloaded = true
-      }
-    },
-    reloadSaleProducts() {
-      let reload = setInterval(() => {
-        this.productsUnloaded ? this.loadSaleProducts() : clearInterval(reload)
-      }, 5000)
-    }
+  created() {
+    this.$store.dispatch('loadSaleProducts')
   }
 }
 </script>
--- Frontend/Pos/src/store/modules/shop.js
+++ Frontend/Pos/src/store/modules/shop.js
@@ -2,42 +2,54 @@ import axiosInstance from "axios";
 
 export default {
   actions: {
-    async fetchCategoryData({ commit }, slug) {
+    async fetchCategoryData({commit}, slug) {
       try {
         await axiosInstance.get(`${this.getters.getServerURL}/shop/category/${slug}/`).then(({data}) => {
           commit('setCategoryData', data)
         })
-      } catch(e) {
+      } catch (e) {
         throw e
       }
     },
-    async fetchLabelData({ commit }, slug) {
+    async fetchLabelData({commit}, slug) {
       try {
         await axiosInstance.get(`${this.getters.getServerURL}/shop/label/${slug}/`).then(({data}) => {
           commit('setLabelData', data)
         })
-      } catch(e) {
+      } catch (e) {
         throw e
       }
     },
-    async fetchBrandData({ commit }, slug) {
+    async fetchBrandData({commit}, slug) {
       try {
         await axiosInstance.get(`${this.getters.getServerURL}/shop/brand/${slug}/`).then(({data}) => {
           commit('setBrandData', data)
         })
-      } catch(e) {
+      } catch (e) {
         throw e
       }
     },
-    async fetchProductDetailData({ commit }, slug) {
+    async fetchProductDetailData({commit}, slug) {
       try {
         await axiosInstance.get(`${this.getters.getServerURL}/shop/product/${slug}/`).then(({data}) => {
           commit('setProductData', data)
         })
-      } catch(e) {
+      } catch (e) {
         throw e
       }
-    }
+    },
+    async loadSaleProducts({commit, state}) {
+      if(!state.saleProducts.length) {
+        try {
+          this.products = await axiosInstance.get(`${this.getters.getServerURL}/shop/home_sale_products/`)
+            .then(({data}) => {
+              commit('setSaleProducts', data.results)
+            })
+        } catch (e) {
+          throw e
+        }
+      }
+    },
   },
   mutations: {
     setCategoryData(state, data) {
@@ -51,18 +63,24 @@ export default {
     },
     setProductData(state, data) {
       state.productDetailData = data
+    },
+    setSaleProducts(state, data) {
+      state.saleProducts = data
     }
+
   },
   state: {
     category: {},
     label: {},
     brand: {},
-    productDetailData: {}
+    productDetailData: {},
+    saleProducts: []
   },
   getters: {
     getCategoryDetail: (state) => state.category,
     getLabelDetail: (state) => state.label,
     getBrandDetail: (state) => state.brand,
     getProductDetailData: (state) => state.productDetailData,
+    getSaleProducts: (state) => state.saleProducts
   }
 }
